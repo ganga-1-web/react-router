ad968dd435519a0d5bba9a0e69273303b32af76f
Gangadhar Reddy <cgangadharreddy9121@gmail.com>
2026-04-19 15:05:02 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Contact = () => {
   return (
     <div className="container">
@@ -8,5 +7,4 @@ const Contact = () => {
     </div>
   );
 };
-
 export default Contact;
