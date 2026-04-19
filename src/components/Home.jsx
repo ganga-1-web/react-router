4656a4e0fa461e635dcc181b58b3df16d2a1c083
Gangadhar Reddy <cgangadharreddy9121@gmail.com>
2026-04-19 15:05:25 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from "react";
-
 const Home = () => {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-
   return (
     <div className="container">
       <h2>Products</h2>
-
       <div className="grid">
         {products.map((p) => (
           <div className="card" key={p.id}>
@@ -25,5 +21,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
