924fc8a7d92c5aa283eb408c9495387d2ed2eb62
Gangadhar Reddy <cgangadharreddy9121@gmail.com>
2026-04-19 15:06:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import Navbar from "./Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
-
 import "./App.css";
-
 const App = () => {
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -49,8 +45,6 @@ const App = () => {
       ),
     },
   ]);
-
   return <RouterProvider router={router} />;
 };
-
 export default App;
